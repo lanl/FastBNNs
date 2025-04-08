75931eef9e38018c8abffe292d234a8e09d5132a
David J. Schodt <dschodt@lanl.gov>
2025-04-08 15:49:55 -0600

    Add test code for polynomial simulations

---
+++ src/simulation/observation.py
@@ -0,0 +1,77 @@
+"""Functionality for simulating observations of random variables."""
+
+from collections.abc import Callable, Iterable
+from typing import Union
+
+import numpy as np
+
+
+def add_read_noise(
+    signal: Union[float, np.array], sigma: Union[float, np.array]
+) -> Union[float, np.array]:
+    """Noisy realization of `signal` (read noise).
+
+    Args:
+        signal: Clean signal to which we add zero-mean Normal read noise.
+        sigma: Standard deviation of zero-mean Normally distributed read noise. Can be
+            homoscedastic (scalar) or heteroscedastic (array matching len(signal)).
+    """
+    return signal + sigma * np.random.randn(*signal.shape)
+
+
+def sensor_noise(
+    signal: Union[float, np.array], sigma: Union[float, np.array]
+) -> Union[float, np.array]:
+    """Noisy realization of `signal` (read noise + shot noise).
+
+    Args:
+        signal: Clean signal to which we add read noise and shot noise.
+        sigma: Standard deviation of zero mean Normally distributed read noise. Can be
+            homoscedastic (scalar) or heteroscedastic (array matching len(signal)).
+    """
+    return np.random.poisson(add_read_noise(signal=signal, sigma=sigma))
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import polynomials
+
+    x = np.linspace(-1.0, 1.0, 100)
+    signal = polynomials.polynomial(x, order=1, coefficients=[0.0, 1.0])
+
+    # Homoscedastic noise:
+    fig, ax = plt.subplots()
+    ax.plot(x, signal, color="r", linewidth=2, label="clean signal")
+    for _ in range(3):
+        ax.plot(
+            x,
+            add_read_noise(
+                signal=signal,
+                sigma=0.1,
+            ),
+            color="k",
+            alpha=0.2,
+        )
+
+    ax.plot([], color="k", alpha=0.2, label="noisy realizations")
+    ax.legend()
+    plt.show()
+
+    # Heteroscedastic noise:
+    fig, ax = plt.subplots()
+    ax.plot(x, signal, color="r", linewidth=2, label="clean signal")
+    noise_func = lambda x: np.sin(2.0 * np.pi * x)
+    for _ in range(3):
+        ax.plot(
+            x,
+            add_read_noise(
+                signal=signal,
+                sigma=noise_func(x),
+            ),
+            color="k",
+            alpha=0.2,
+        )
+
+    ax.plot([], color="k", alpha=0.2, label="noisy realizations")
+    ax.legend()
+    plt.show()
---
+++ src/simulation/polynomials.py
@@ -0,0 +1,22 @@
+"""Functionality for simulating polynomial data."""
+
+from collections.abc import Iterable
+from typing import Union
+
+import numpy as np
+
+
+def polynomial(
+    x: Union[float, np.array], order: int = 1, coefficients: Iterable = [0.0, 1.0]
+) -> Union[float, np.array]:
+    """Basic polynomial."""
+    return np.sum([c * (x**n) for c, n in zip(coefficients, range(order + 1))], axis=0)
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    x = np.linspace(-1.0, 1.0, 1000)
+    fig, ax = plt.subplots()
+    ax.plot(x, polynomial(x))
+    plt.show()
