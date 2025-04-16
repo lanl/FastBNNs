Fix some issues with KL calculations

src/bnn/layers.py
@@ -147,12 +147,12 @@ class BayesianLayer(torch.nn.Module):
         }
 
     def compute_kl_divergence(
-        self, prior: Union[dict, dist.Distribution] = None, n_samples: int = 1
+        self, priors: Union[dict, dist.Distribution] = None, n_samples: int = 1
     ) -> torch.tensor:
         """Compute the KL divergence between self.prior and module parameters.
 
         Args:
-            prior: Prior distribution over parameters.  This can be a single
+            priors: Prior distribution over parameters.  This can be a single
                 distribution for all parameters or a dictionary whose keys
                 correspond to named parameters of this layer.  By default, None
                 will use self.priors.  This argument is used to allow external
@@ -161,11 +161,15 @@ class BayesianLayer(torch.nn.Module):
                 divergence for distributions not compatible with
                 torch.nn.distributions.kl_divergence()
         """
+        # Return 0.0 if no learnable parameters are in this layer.
+        if len([p for p in self.parameters()]) == 0:
+            return torch.tensor(0.0)
+
         # Reorganize priors if needed.
-        if prior is None:
+        if priors is None:
             priors = self.priors
-        elif isinstance(prior, dist.Distribution):
-            priors = {key: prior for key in self.samplers.keys()}
+        elif not isinstance(priors, dict):
+            priors = {key: priors for key in self.samplers.keys()}
 
         # Loop over parameters and compute KL contribution.
         device = next(self._module_params.values()).device

src/bnn/losses.py
@@ -11,32 +11,20 @@ def kl_divergence_sampled(
     dist0: dist.Distribution, dist1: dist.Distribution, n_samples: int = 1
 ):
     """KL divergence KL(dist0||dist1) approximated by Monte Carlo sampling."""
-    kl_divergence = torch.tensor(0.0)
+    kl_divergence = []
     for _ in range(n_samples):
         sample = dist0.sample()
-        kl_divergence += dist0.log_prob(sample) - dist1.log_prob(sample)
+        kl_divergence.append(dist0.log_prob(sample) - dist1.log_prob(sample))
 
-    return kl_divergence / n_samples
+    return torch.stack(kl_divergence).mean(dim=0)
 
 
 class KLDivergence(_Loss):
     """KL divergence loss for Bayesian neural networks."""
 
-    def __init__(self):
-        """Initialize KL divergence loss."""
-        super().__init__()
-
-    def forward(
-        self,
-        model: torch.nn.Module,
-        prior: Union[dict, dist.Distribution] = None,
-    ) -> torch.tensor:
-        """Compute KL divergence for Bayesian sub-modules of `model`.
-
+    def __init__(self, prior: Union[dict, dist.Distribution] = None):
+        """Initialize KL divergence loss.
         Args:
-            model: torch.nn.Module that may have some Bayesian layers
-                as sub-modules, for which we'll compute the KL divergence w.r.t
-                their prior.
             prior: Prior distribution over parameters.  This can be a single
                 distribution for all parameters or a dictionary of dictionaries whose
                 primary keys correspond to named modules and whose secondary keys
@@ -45,14 +33,33 @@ class KLDivergence(_Loss):
                 will use priors set within each Bayesian layer on
                 initialization.
         """
+        super().__init__()
+        self.prior = prior
+
+    def forward(
+        self,
+        model: torch.nn.Module,
+    ) -> torch.tensor:
+        """Compute KL divergence for Bayesian sub-modules of `model`.
+
+        Args:
+            model: torch.nn.Module that may have some Bayesian layers
+                as sub-modules, for which we'll compute the KL divergence w.r.t
+                their prior.
+        """
         kl = []
-        for m, module in enumerate(model.named_modules()):
-            if hasattr(module[1], "compute_kl_divergence"):
-                if isinstance(prior, dict):
+        for module in model.named_modules():
+            module_parameters = [p for p in module[1].parameters()]
+            if hasattr(module[1], "compute_kl_divergence") and (
+                len(module_parameters) > 0
+            ):
+                if isinstance(self.prior, dict):
                     # Pass the input prior dictionary for this module.
-                    kl.append(module[1].compute_kl_divergence(prior[module[0]]))
+                    kl.append(
+                        module[1].compute_kl_divergence(priors=self.prior[module[0]])
+                    )
                 else:
-                    kl.append(module[1].compute_kl_divergence(prior))
+                    kl.append(module[1].compute_kl_divergence(priors=self.prior))
 
         return torch.stack(kl).sum()
 

src/bnn/priors.py
@@ -1,38 +1,21 @@
 """Definitions of prior distributions over neural network parameters."""
 
-from collections.abc import Callable, Iterable
+from collections.abc import Iterable
 
-import numpy as np
 import torch
 import torch.distributions as dist
 
 
-class Prior(torch.nn.Module):
-    def __init__(self):
-        """Initializer for Prior base class."""
-        super().__init__()
-
-    def log_prob(self, x: torch.tensor) -> torch.tensor:
-        """Compute the log PDF of the prior at points `x`."""
-        pass
-
-    def sample(self, sample_shape: Iterable) -> torch.tensor:
-        """Generate samples from the prior of size `sample_shape`."""
-        pass
-
-
-class SpikeSlab(Prior):
+class SpikeSlab(dist.Distribution):
     """Spike-slab Gaussian Mixture Model prior."""
 
     def __init__(
         self,
-        mu: torch.tensor = torch.tensor([0.0, 0.0]),
-        sigma: torch.tensor = torch.tensor([0.1, 1.0]),
+        loc: torch.tensor = torch.tensor([0.0, 0.0]),
+        scale: torch.tensor = torch.tensor([0.1, 1.0]),
         probs: torch.tensor = torch.tensor([0.5, 0.5]),
     ):
         super().__init__()
-        loc = torch.nn.Parameter(mu, requires_grad=False)
-        scale = torch.nn.Parameter(sigma, requires_grad=False)
         mixture_distribution = dist.Categorical(probs=probs)
         self.distribution = dist.MixtureSameFamily(
             mixture_distribution=mixture_distribution,