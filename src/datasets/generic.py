1b872d7627b7df234ad505ea19d0dda25a82d616
David J. Schodt <dschodt@lanl.gov>
2025-08-20 13:19:05 -0600

    Add caching to datasets

--- src/datasets/generic.py
+++ src/datasets/generic.py
@@ -15,6 +15,7 @@ class SimulatedData(torch.utils.data.Dataset):
         self,
         data_generator: Callable,
         dataset_length: int = 1,
+        cache: bool = False,
         transform: Optional[torch.nn.Module] = None,
     ) -> None:
         """Initializer for stochastic simulator dataset.
@@ -22,20 +23,29 @@ class SimulatedData(torch.utils.data.Dataset):
         Args:
             data_generator:  Callable that can be called to generate data as data = data_generator()
             dataset_length: Desired dataset length (set manually since `simulator` is stochastic).
+            cache: Flag indicating data should be cached in memory (as opposed to sampling new
+                data every __getitem__()).
             transform: Transform applied to simulated data before returning.
         """
         super().__init__()
         self.data_generator = data_generator
         self._len = dataset_length
+        self.cache = cache
         self.transform = transform
+        self.data = [None for _ in range(dataset_length)]
 
     def __len__(self) -> int:
         return self._len
 
     def __getitem__(self, idx: int) -> dict:
-        data_dict = self.data_generator()
-        if self.transform is not None:
-            data_dict["output"] = self.transform(data_dict["output"])
+        if self.cache and self.data[idx] is not None:
+            return self.data[idx]
+        else:
+            data_dict = self.data_generator()
+            if self.transform is not None:
+                data_dict["output"] = self.transform(data_dict["output"])
+            if self.cache:
+                self.data[idx] = data_dict
 
         return data_dict
 
--- src/datasets/polynomial.py
+++ src/datasets/polynomial.py
@@ -13,6 +13,7 @@ class Polynomial(torch.utils.data.Dataset):
         self,
         data_generator: Callable,
         dataset_length: int = 1,
+        cache: bool = False,
         transform: Optional[torch.nn.Module] = None,
     ) -> None:
         """Initializer for stochastic simulator dataset.
@@ -26,13 +27,20 @@ class Polynomial(torch.utils.data.Dataset):
         self.data_generator = data_generator
         self._len = dataset_length
         self.transform = transform
+        self.cache = cache
+        self.data = [None for _ in range(dataset_length)]
 
     def __len__(self) -> int:
         return self._len
 
     def __getitem__(self, idx: int) -> tuple:
-        data_dict = self.data_generator()
-        if self.transform is not None:
-            data_dict["output"] = self.transform(data_dict["output"])
+        if self.cache and self.data[idx] is not None:
+            return self.data[idx]
+        else:
+            data_dict = self.data_generator()
+            if self.transform is not None:
+                data_dict["output"] = self.transform(data_dict["output"])
+            if self.cache:
+                self.data[idx] = (data_dict["input"]["x"], data_dict["output"])
 
         return data_dict["input"]["x"], data_dict["output"]
